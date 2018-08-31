2fd7945ef9f310368d24d3805503482ea38f5d18
TFS2013\yaohongjie <1508728331@qq.com>
2018-08-31 16:52:07 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <index></index>
+  </div>
+</template>
+
+<script>
+import Index from './components/Index.vue'
+export default {
+  name: 'app',
+  components: {
+    Index
+  }
+}
+</script>
+
+<style>
+</style>
